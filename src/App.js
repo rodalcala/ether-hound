4a9cbe487fa9197beb8b63a25868ab2d72ccb9f1
Rodrigo Alcala <alcala@myego2go.de>
2019-10-05 21:09:27 +0200

    feat: add a app loader

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { Main } from '@aragon/ui';
+import { Main, theme } from '@aragon/ui';
+import { PropagateLoader } from 'react-spinners';
 
 import './App.css';
 import actions from './redux/actions';
 import Header from './components/Header';
 import Charts from './components/Charts';
 
-const App = ({ getBlocks }) => {
+const App = ({ getBlocks, isAppReady }) => {
   const [nBlocks, setNBlocks] = useState(10);
 
   useEffect(() => {
     getBlocks(nBlocks)
   }, [getBlocks, nBlocks])
 
-  return (
-    <Main className={'main'}>
-      <Header nBlocks={nBlocks} setNBlocks={setNBlocks} />
-      <Charts nBlocks={nBlocks} />
-    </Main>
-  )
+  if (isAppReady) {
+    return (
+      <Main className='main'>
+        <Header nBlocks={ nBlocks } setNBlocks={ setNBlocks } />
+        <Charts nBlocks={ nBlocks } />
+      </Main>
+    )
+  } else {
+    return (
+      <div className='loading'>
+        <PropagateLoader size={ 20 } color={ theme.gradientEndActive } />
+      </div>
+    )
+  }
 }
+
+const mapStateToProps = state => ({
+  isAppReady: state.isAppReady
+})
+
 const mapDispatchToProps = dispatch => ({
   getBlocks: (nBlocks) => dispatch(actions.getBlocks(nBlocks)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,17 +1,21 @@
 import Web3 from 'web3';
-import { GET_BLOCKS_SUCCESS } from './constants';
+import { GET_BLOCKS_PENDING, GET_BLOCKS_SUCCESS } from './constants';
 
 const web3 = new Web3(Web3.givenProvider || null /* TO-DO: add a fallback here */ );
 
 const initialState = {
   web3,
   blocks: [],
+  activeBlock: undefined,
+  isAppReady: false,
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_BLOCKS_PENDING:
+      return { ...state, isAppReady: false }
     case GET_BLOCKS_SUCCESS:
-      return { ...state, blocks: action.blocks }
+      return { ...state, blocks: action.blocks, isAppReady: true }
     default:
       return state;
   }
